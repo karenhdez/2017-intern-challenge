d7fe93d81821427c0864b2a3e17001954bef38ac
karenhdez <karenhdez2017@gmail.com>
2017-02-07 17:00:37 -0600

    Completed 2 Unit Tests
    Completed testGetProductTotal() and testProductTotalEqualsTransactionTotal()

--- tests/CashRegisterTest.php
+++ tests/CashRegisterTest.php
@@ -33,11 +33,21 @@ class CashRegisterTest extends \PHPUnit\Framework\TestCase
 
     public function testGetProductTotal()
     {
-        
+        $total = $this->cashRegister->getProductTotal($this->order);
+
+        $this->assertEquals(\Money\Money::class, get_class($total));
+        $this->assertEquals('1000', $total->getAmount());
+        $this->assertEquals('USD', $total->getCurrency()->getCode());
     }
 
     public function testProductTotalEqualsTransactionTotal()
     {
-        
+        $productTotal = $this->cashRegister->getProductTotal($this->order);
+        $transTotal = $this->cashRegister->getTransactionTotal($this->order);
+
+        $this->assertEquals($productTotal, $transTotal);
+        $this->assertEquals(get_class($productTotal), get_class($transTotal));
+        $this->assertEquals($productTotal->getAmount(), $transTotal->getAmount());
+        $this->assertEquals($productTotal->getCurrency()->getCode(), $transTotal->getCurrency()->getCode());  
     }
 }
